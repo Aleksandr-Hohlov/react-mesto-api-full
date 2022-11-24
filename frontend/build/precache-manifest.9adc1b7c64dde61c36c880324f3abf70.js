3f4aab94da9e3fcbc53b971d8291088fb956b77d
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-11-25 02:20:26 +0300

    Add files via upload

---
+++ frontend/build/precache-manifest.9adc1b7c64dde61c36c880324f3abf70.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "de05b4e57a58ad23578b",
+    "url": "/static/css/main.5191518e.chunk.css"
+  },
+  {
+    "revision": "de05b4e57a58ad23578b",
+    "url": "/static/js/main.de05b4e5.chunk.js"
+  },
+  {
+    "revision": "6336b426631f80f6c7a2",
+    "url": "/static/js/1.6336b426.chunk.js"
+  },
+  {
+    "revision": "bbd603416ebeae9b82e5",
+    "url": "/static/js/2.bbd60341.chunk.js"
+  },
+  {
+    "revision": "0979ea937a1ee6514631",
+    "url": "/static/js/runtime~main.0979ea93.js"
+  },
+  {
+    "revision": "7baba3c1428ee943536a8022d42f0985",
+    "url": "/static/media/logo.7baba3c1.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/static/media/imageSuccess.e38b395c.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/static/media/imageError.4072126c.svg"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "/static/media/Inter-Black.661569af.woff2"
+  },
+  {
+    "revision": "eea97ded621cd8a18067c7243b19ceea",
+    "url": "/static/media/Inter-Medium.eea97ded.woff2"
+  },
+  {
+    "revision": "66f0ef6196f8d81ced9930f6675035a1",
+    "url": "/static/media/Inter-Medium.66f0ef61.woff"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "eec224cbddb8500ac62361155b34f265",
+    "url": "/static/media/like.eec224cb.svg"
+  },
+  {
+    "revision": "3d813f810a41032c140568c878fd7d85",
+    "url": "/static/media/like_active.3d813f81.svg"
+  },
+  {
+    "revision": "cb0f2bbfd915a7bfa586c3a3bd2efc48",
+    "url": "/static/media/Close_Icon.cb0f2bbf.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/static/media/edit.6bfdb354.svg"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/static/media/add.b2fce4ac.svg"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "/static/media/Inter-Regular.dc131113.woff2"
+  },
+  {
+    "revision": "4831282e649d143ce022040beb49ad4f",
+    "url": "/static/media/Delete_Icon.4831282e.svg"
+  },
+  {
+    "revision": "28718e5c042769efe8bcf2074935af79",
+    "url": "/index.html"
+  }
+];
